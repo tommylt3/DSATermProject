ea5d33300aafe99f2bae6a9dac11071c90d556c4
Gurtygurt98 <81448848+Gurtygurt98@users.noreply.github.com>
2023-04-14 17:33:29 -0400

    Update RadixTree.java

--- src/java/RadixTree.java
+++ src/java/RadixTree.java
@@ -167,4 +167,9 @@ public class RadixTree {
         else            // number
             return index - 48;
     }
+    
+    // add word without calling the root 
+    public void addWord(String word, int freq) {
+       this.addWord(root, word, freq);
+    }
 }
